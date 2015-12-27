982631f7156422edbf33b4a658cfb7b59b445e4a
Kento Yamakawa <kenton0203@gmail.com>
2015-12-27 06:38:42 +0000

    add user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220075346) do
+ActiveRecord::Schema.define(version: 20151227060426) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151220075346) do
     t.string   "movie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
